3d79d61598f06deb87d576550eb32f7dc097018f
Anshul Sachdev <anshul.sachdev2016@vitstudent.ac.in>
2018-02-11 23:35:50 +0530

    Update dodge-it.py

--- dodge-it.py
+++ dodge-it.py
@@ -97,32 +97,6 @@ def quitgame():
     quit()
 
 
-def unpause():
-    global pause
-    pause = False
-
-
-def paused():
-    largeText = pygame.font.SysFont("comicsansms", 115)
-    TextSurf, TextRect = text_objects("Paused", largeText)
-    TextRect.center = ((display_width / 2), (display_height / 2))
-    gameDisplay.blit(TextSurf, TextRect)
-
-    while pause:
-        for event in pygame.event.get():
-            # print(event)
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-        # gameDisplay.fill(white)
-
-        button("Continue", 150, 450, 100, 50, green, bright_green, unpause)
-        button("Quit", 550, 450, 100, 50, red, bright_red, quitgame)
-
-        pygame.display.update()
-        clock.tick(15)
-
 
 def game_intro():
     intro = True
